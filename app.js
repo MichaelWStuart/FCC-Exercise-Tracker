2fcebb72f6098af138af8fcd627d2d1e3e30c3b1
Michael Stuart <mikewstuart@gmail.com>
2017-02-21 17:09:26 -0800

    set up for heoku w mongolab

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT || 8080;
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
+if(process.env.node_env !== 'production') {
+  require('dotenv').load();
+  serverAddress = '';
+}
+
+const dbURL = `mongodb://${process.env.username}:${process.env.password}@${process.env.mdbloc}:${process.env.mdbport}/url_shortener`;
+
 
 app.get('/', (req,res) => {
   res.render('index');
